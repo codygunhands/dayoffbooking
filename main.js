af748d9d6015783ae435e1cfd8f93f770bd90515
codygunhands <109570198+codygunhands@users.noreply.github.com>
2023-11-05 12:25:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+// main.js
+window.onload = function () {
+    initializeLiff(); // Initialize LIFF
+};
 
+function initializeLiff() {
+    liff.init({
+        liffId: "YOUR_LIFF_ID" // Replace with your LIFF ID
+    }).then(() => {
+        if (!liff.isLoggedIn()) {
+            liff.login(); // Log the user in if they are not logged in
+        }
+        // You can now get the user profile, etc.
+        // liff.getProfile().then(profile => { ... });
+    }).catch(err => {
+        console.error('LIFF initialization failed', err);
+    });
+}
+
+document.getElementById('reservation-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const date = document.getElementById('date').value;
+    const time = document.getElementById('time').value;
+
+    // Here you would typically send this data to your server for processing.
+    console.log('Reservation details:', { name, date, time });
+});
